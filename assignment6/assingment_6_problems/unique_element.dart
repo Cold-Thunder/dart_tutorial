66861b8b8d3bf3af7e959a3658e2e284d268e43e
Md.Hemal <mdhemal202000@gmail.com>
2024-11-07 15:55:36 +0600

    unique item finder done

--- assignment6/assingment_6_problems/unique_element.dart
+++ assignment6/assingment_6_problems/unique_element.dart
@@ -1,53 +1,52 @@
 import 'dart:io';
 
-void main(){
+void main() {
   late List<int> numbers;
-  print('Please enter the length: ');
-  int? length = int.tryParse(stdin.readLineSync()!);
-  // calling element taker
-  if(length != null){
-    numbers = getting_elem(length);
-  }else{
-    print('Please enter a valid length!');
-  }
+  late List<int> unique_number;
+  // calling list item taker
+  numbers = list_taker();
+  print(numbers);
 
-  int unique = finder(numbers);
-  print(unique);
+  // calling unique item finder
+  if (numbers.isNotEmpty) {
+    unique_number = unique_item(numbers);
+  } else {
+    print('Please enter list items!');
+  }
+  print(unique_number);
 }
 
-// list item taking mathod
-List<int> getting_elem(int length){
-  List<int> numbers = [];
-  print("Please enter the list elements: ");
-  for(int i =1; i <= length; i++){
-    int? data = int.tryParse(stdin.readLineSync()!);
-    if(data != null){
-      numbers.add(data);
-    }else{
-      print('Please enter a numbers!');
-      i--; //decreasing index 1
-    }
-  }
+//list item taker
+List<int> list_taker() {
+  late List<int> numbers;
+  print("Please seperate items with (' ')");
+  String num_string = stdin.readLineSync()!;
+  numbers = num_string.split(' ').map((item) {
+    int num = int.tryParse(item) ?? 0;
+    return num;
+  }).toList();
+
   return numbers;
 }
 
 // unique item finder
-int finder(List<int> numbers){
-  late int unique;
-  List<int> unique_data = [];
-  List<int> duplicated_items = [];
+List<int> unique_item(List<int> numbers) {
+  List<int> unique_num = [];
+  List<int> duplicates = [];
 
-  for(int i =0; i< numbers.length; i++){
-    for(int j = i+1; j < numbers.length; j++){
-      if(numbers[i] != numbers[j]){
-        unique = numbers[i];
-        unique_data.add(unique);
-        break;
+  for (int i = 0; i < numbers.length; i++) {
+    for (int j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] == numbers[j]) {
+        duplicates.add(numbers[i]);
       }
     }
-    if(unique_data.isNotEmpty){
-      break;
+  }
+  for (int i = 0; i < numbers.length; i++) {
+    // if duplicates is not contains the element
+    if (!duplicates.contains(numbers[i])) {
+      unique_num.add(numbers[i]);
     }
   }
-  return unique;
+
+  return unique_num;
 }
